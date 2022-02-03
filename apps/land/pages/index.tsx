33f8f51439629d3d156413fa8aa532db847a4f18
Rajiv Seelam <rajiv@betalectic.com>
2022-02-03 13:23:33 +0530

    Log env variable

--- apps/land/pages/index.tsx
+++ apps/land/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { Button } from "ui";
 
 const Home: NextPage = () => {
-  console.log("process.env", process.env);
+  console.log("process.env", process.env.NEXT_PUBLIC_API_URL);
 
   return (
     <div className="my-12 flex flex-col items-center max-w-2xl mx-auto">
