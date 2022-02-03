a5cea834ef07a9826fffd16c2c5e48725f779d4a
Rajiv Seelam <rajiv@betalectic.com>
2022-02-03 13:18:55 +0530

    Add env variable

--- apps/land/pages/index.tsx
+++ apps/land/pages/index.tsx
@@ -5,9 +5,10 @@ const Home: NextPage = () => {
   return (
     <div className="my-12 flex flex-col items-center max-w-2xl mx-auto">
       <Button />
-      <h1 className="mt-6 text-3xl font-bold underline bg-slate-200 text-red-500">
+      <h1 className="my-6 text-3xl font-bold underline bg-slate-200 text-red-500">
         Hello world! Welcome to land.
       </h1>
+      <pre> {process.env.NEXT_PUBLIC_API_URL}</pre>
     </div>
   );
 };
